24f8c6b5d9154703ae1e410e33415eb396fb7677
chandelyash2 <chandel.yash2@gmail.com>
2023-10-09 11:18:31 +0530

    delete

--- server/Controllers/favoriteVehicle/removeFavoriteVehicleController.ts
+++ server/Controllers/favoriteVehicle/removeFavoriteVehicleController.ts
@@ -1,7 +1,7 @@
 import FavoriteVehcileModel from '@/Database/models/favoriteVehicle'
 
 export default async (input, user) => {
-  const vehicle = await FavoriteVehcileModel.findByIdAndDelete({
+  const vehicle = await FavoriteVehcileModel.findOneAndDelete({
     vehicle: input.vehicle,
     user: user,
   })
