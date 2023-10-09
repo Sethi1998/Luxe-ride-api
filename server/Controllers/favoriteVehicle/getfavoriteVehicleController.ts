b479599b60d947871e76e88ec1ed0ccebfb0ee11
chandelyash2 <chandel.yash2@gmail.com>
2023-10-09 12:52:37 +0530

    delete

--- server/Controllers/favoriteVehicle/getfavoriteVehicleController.ts
+++ server/Controllers/favoriteVehicle/getfavoriteVehicleController.ts
@@ -1,10 +1,42 @@
 import FavoriteVehcileModel from '@/Database/models/favoriteVehicle'
+import mongoose from 'mongoose'
 
 export default async (user) => {
-  const vehciles = await FavoriteVehcileModel.find({ user: user })
-    .populate('vehicle')
-    .populate('user')
-    .lean()
+  const vehciles = await FavoriteVehcileModel.aggregate([
+    {
+      $match: {
+        user: new mongoose.Types.ObjectId(user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: 'vehicles',
+        localField: 'vehicle',
+        foreignField: '_id',
+        as: 'vehicleData',
+      },
+    },
+    { $unwind: '$vehicleData' },
+    {
+      $lookup: {
+        from: 'vehiclecompanies',
+        localField: 'vehicleData.make',
+        foreignField: '_id',
+        as: 'companyData',
+      },
+    },
+    {
+      $lookup: {
+        from: 'vehiclesubcategories',
+        localField: 'vehicleData.model',
+        foreignField: '_id',
+        as: 'modelData',
+      },
+    },
+    { $unwind: '$companyData' },
+    { $unwind: '$modelData' },
+  ])
+
   if (vehciles.length > 0) {
     return {
       data: vehciles,
